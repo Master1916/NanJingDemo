688215f88fcf13d3074900b3a3d98148ce2a89a8
Master1916 <zppzzl@gmail.com>
2013-06-18 17:41:55 +0800

    2013年6月18日17:41:43 提交

--- gen/com/litsoft/nanjingdemo/R.java
+++ gen/com/litsoft/nanjingdemo/R.java
@@ -20,7 +20,8 @@ public final class R {
         public static final int app_name=0x7f050000;
     }
     public static final class xml {
-        public static final int phonegap=0x7f040000;
-        public static final int plugins=0x7f040001;
+        public static final int config=0x7f040000;
+        public static final int phonegap=0x7f040001;
+        public static final int plugins=0x7f040002;
     }
 }
--- src/com/litsoft/nanjingdemo/MyPhoneGapActivity.java
+++ src/com/litsoft/nanjingdemo/MyPhoneGapActivity.java
@@ -1,12 +1,73 @@
 package com.litsoft.nanjingdemo;
 
-import   com.phonegap.*;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
+
+import com.phonegap.DroidGap;
 
 public class MyPhoneGapActivity extends DroidGap {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		super.loadUrl("file:///android_asset/www/index.html");
+		super.loadUrl("file:///android_asset/www/index2.html");
 	}
+
+	/*protected void dialog() {
+
+		AlertDialog.Builder builder = new Builder(MyPhoneGapActivity.this);
+
+		builder.setMessage("确定要退出吗?");
+
+		builder.setTitle("提示");
+
+		builder.setPositiveButton("确认",
+
+		new android.content.DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+
+				dialog.dismiss();
+
+				MyPhoneGapActivity.this.finish();
+
+			}
+
+		});
+
+		builder.setNegativeButton("取消",
+
+		new android.content.DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+
+				dialog.dismiss();
+
+			}
+
+		});
+
+		builder.create().show();
+
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+
+			dialog();
+
+			return false;
+
+		}
+
+		return false;
+
+	}*/
+
 }
